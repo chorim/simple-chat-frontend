d5b6d2ec0906dc120c2a270c192415438568d461
유키유키 <pib1t@naver.com>
2019-06-02 02:34:14 +0900

    Fix typo

--- components/TopHeader.vue
+++ components/TopHeader.vue
@@ -4,7 +4,7 @@
 		<b-nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
 			<div class="container">
 				<b-navbar class="navbar-brand" href="/">YUKIYUKI CHATROOM</b-navbar>
-				<b-navbar-toggle class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<b-navbar-toggle class="navbar-toggler" type="button" data-toggle="collapse" target="navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</b-navbar-toggle>
 				<b-collapse class="collapse navbar-collapse" id="navbarResponsive">
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,50 @@
 <template>
-	<h1 class="my-4 text-center text-lg-left">Login</h1>
+	<div>
+		<div class="row">
+			<h1 class="my-4 text-center text-lg-left">Login</h1>
+			<b-form @submit.prevent="onSubmit" class="form-horizontal col-12">
+				<div class="form-group">
+					<b-col sm="12">
+						<b-form-input
+							v-model="form.username"
+							name="username"
+							id="username"
+							class="form-control input-lg"
+							placeholder="Username"
+							required></b-form-input>
+					</b-col>
+				</div>
+
+				<div class="form-group">
+					<b-col sm="12">
+						<b-button variant="primary" class="btn-block" id="loginBtn" type="submit">Login</b-button>
+					</b-col>
+				</div>
+			</b-form>
+		</div>
+	</div>
 </template>
 
 <script>
+import socket from '~/plugins/socket.io'
 
 export default {
+  data() {
+    return {
+      form: {
+        username: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
 
+	    socket.emit('login', {username: this.form.username})
+	    this.$router.replace('/list')
+
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
 
