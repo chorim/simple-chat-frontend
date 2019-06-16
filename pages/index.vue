3d4258194673df3b5946c0afdb29ae56f5da2ef7
Yukyuki <pib1t@naver.com>
2019-06-16 18:03:21 +0900

    Added requestRoom

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,20 @@
 	<div>
 		<div class="row">
       <h1 class="my-4 text-lg-left">Room ( Logged in <span style="color: #ff0000">{{ me.username }}</span> )</h1>
+      <div class="col-12">
+        <table class="table table-hover">
+          <thead>
+          <tr>
+            <th scope="col">제목</th>
+          </tr>
+          </thead>
+          <tbody id="roomList">
+          </tbody>
+        </table>
+      </div>
+      <div class="col-12">
+        <button type="button" class="btn btn-primary btn-block" @click="requestRoom">Craete Room</button>
+      </div>
 		</div>
 	</div>
 </template>
@@ -11,12 +25,19 @@
   export default {
     middleware: 'auth',
     beforeMount() {
-      socket.emit('requestRoomList')
-      console.log('requestRoomList called')
+      this.requestRoom()
     },
     computed: {
       me () {
         return this.$auth.user
+      }
+    },
+    methods: {
+      requestRoom() {
+        socket.emit('requestRoomList')
+
+        var roomName = prompt('Please input room name to create')
+        console.log('requestRoomList called')
       }
     }
   }
