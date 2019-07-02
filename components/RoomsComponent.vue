898571dc3e68e9936c8897486affa8a367b2496b
Yukyuki <pib1t@naver.com>
2019-07-02 16:13:35 +0900

    Added RoomsComponent

---
+++ components/RoomsComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="col-12">
+    <table class="table table-hover">
+      <thead>
+      <tr>
+        <th scope="col">제목</th>
+      </tr>
+      </thead>
+      <tbody id="roomList">
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['rooms']
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,7 @@
 	<div>
 		<div class="row">
       <h1 class="my-4 text-lg-left">Room ( Logged in <span style="color: #ff0000">{{ myInfo.username }}</span> )</h1>
-      <div class="col-12">
-        {{ $store.state.token }}
-        <table class="table table-hover">
-          <thead>
-          <tr>
-            <th scope="col">제목</th>
-          </tr>
-          </thead>
-          <tbody id="roomList">
-          </tbody>
-        </table>
-      </div>
+      <rooms-component :rooms="rooms"/>
       <div class="col-12">
         <button type="button" class="btn btn-primary btn-block" @click="createRoom">Craete Room</button>
       </div>
@@ -23,13 +12,22 @@
 
 <script>
   import socket from '~/plugins/socket.io'
-  console.log("TpyeD?", typeof socket)
+  import RoomsComponent from '~/components/RoomsComponent.vue'
   import { mapGetters } from 'vuex'
   export default {
     middleware: 'auth',
+    data() {
+      return {
+        rooms: []
+      }
+    },
+    components: { RoomsComponent },
     beforeMount() {
       this.requestRoom()
 	    console.log(this.$socket)
+      this.$socket.on('newRoom', (message) => {
+
+      })
     },
     computed: {
       ...mapGetters(['myInfo', 'getToken', 'getSocket'])
